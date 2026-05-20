f511491f8352dbd9d4fb7e31b7b0f46eb24960ad
bajanchis20-png <Bajanchis20@gmail.com>
2026-05-20 11:30:19 -0400

    new

---
+++ src/routes/metodos-de-pago/+page.svelte
@@ -0,0 +1,15 @@
+<section>
+    <div>
+        <p>transsacciones seguras</p>
+        <p>poder total en tus pagos</p>
+        <p>Equipate con lo mejor sin esperas. nuestro ecosistema de pagos esta diseñado para la maxima velocidad, garatizando que tus activos lleguen a tu inventario en tiempo record.</p>
+    </div>
+
+    <div>
+        <img src="" alt="">
+        <div>
+            <img src="" alt="">
+            <p>portecion nivel leyenda</p>
+        </div>
+    </div>
+</section>
---
+++ src/routes/seguridad-garantia/+page.svelte
@@ -0,0 +1,4 @@
+<section>
+    <p>seguridad y garantia</p>
+    <p>forjando confianza en cada transaccion.Tu equipo legendario,protegido por los metodos de entrega mas seguros del reino.</p>
+</section>
