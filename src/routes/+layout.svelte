ca9e592665d8c8cc61552be7ee7601bed3fe52d3
bajanchis20-png <Bajanchis20@gmail.com>
2026-05-20 10:50:37 -0400

    ebgames

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,24 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+
+<nav>
+
+	<div>
+		<a href="/catalogo">catalogo</a>
+		<a href="/seguridad-garantia">seguridad</a>
+		<a href="/metodos-de-pago">metodos de pago</a>
+	</div>
+
+	<div>
+		<button>login</button>
+		<button>whatsaap</button>
+	</div>
+
+
+</nav>
+
+
+
 {@render children()}
