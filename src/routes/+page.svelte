9f8b1becfbbabbf76865e8887aaf96b63f6f99ee
bajanchis20-png <Bajanchis20@gmail.com>
2026-05-22 10:28:55 -0400

    Cambios metodos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
-
 <section>
-<img src="/logo.jpg" alt="">
+    <img src="/logo.jpg" alt="" />
 
+    <div>
+        <p>EQUIPATE COMO UN DIOS</p>
+        <p>Los mejores ìtems de metin2 al alcance de tus manos,seguridad,rapidez y confianza</P>
+    </div>
 </section>
--- src/routes/metodos-de-pago/+page.svelte
+++ src/routes/metodos-de-pago/+page.svelte
@@ -2,14 +2,45 @@
     <div>
         <p>transsacciones seguras</p>
         <p>poder total en tus pagos</p>
-        <p>Equipate con lo mejor sin esperas. nuestro ecosistema de pagos esta diseñado para la maxima velocidad, garatizando que tus activos lleguen a tu inventario en tiempo record.</p>
+        <p>
+            Equipate con lo mejor sin esperas. nuestro ecosistema de pagos esta
+            diseñado para la maxima velocidad, garatizando que tus activos
+            lleguen a tu inventario en tiempo record.
+        </p>
     </div>
 
     <div>
-        <img src="/imagendepagos.jpg" alt="">
+        <img src="/imagendepagos.jpg" alt="" />
         <div>
-            <img src="/escudito.png" alt="">
+            <img src="/escudito.png" alt="" />
             <p>portecion nivel leyenda</p>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div>
+        <p>Cobertura global</p>
+        
+        <p>Pagos por pais</p>
+    </div>
+
+    <div>
+        <div>
+            <img src="/paypal_PNG7.png" alt="" />
+            <p>Paypal</p>
+        </div>
+        <div>
+            <img src="/wenster.png" alt="" />
+            <p>Wenster Union</p>
+        </div>
+        <div>
+            <img src="/ria.webp" alt="" />
+            <p>Ria</p>
+        </div>
+        <div>
+            <img src="/binance.png" alt="">
+            <p>Binance</p>
         </div>
     </div>
 </section>
