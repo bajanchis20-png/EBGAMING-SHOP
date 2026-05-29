caf9e373c9c79272c7fa4f91ecd56e283351ccb5
bajanchis20-png <Bajanchis20@gmail.com>
2026-05-29 10:35:26 -0400

    listo el incio

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,20 +8,27 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<nav>
-	<div>
+<nav class="flex justify-between items center px-12 py-4 bg-red-950">
+	<div class="text-lg font-semibold flex gap-5 text-white">
+		<a href="/">
+			<img class="rounded-full" src="/logofondox.png" alt="" width={70} />
+		</a>
 		<a href="/catalogo">catalogo</a>
 		<a href="/seguridad-garantia">seguridad</a>
 		<a href="/metodos-de-pago">metodos de pago</a>
 	</div>
 
-	<div>
-		<button>
-			login
+	<div class="flex gap-5">
+		<button
+			class="flex text-lg font-semibold text-white border-2 border-red-300 px-6 py-1.5"
+		>
+			Login
 			<Icon icon="glyphs-poly:user" width={24} height={24} />
 		</button>
-		<button>
-			whatsaap
+		<button
+			class="flex text-lg font-semibold text-white border-2 border-red-300 px-6 py-1.5 bg-red-300"
+		>
+			WhatsApp
 			<Icon icon="logos:whatsapp-icon" width={24} height={24} />
 		</button>
 	</div>
