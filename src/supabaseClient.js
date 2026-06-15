edd3de2fa335c62e4c508d8d6c8dc26f7282b231
bajanchis20-png <Bajanchis20@gmail.com>
2026-06-15 14:10:30 -0400

    new

---
+++ src/supabaseClient.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+import { supabase } from '$lib/supabaseClient';
+
+// Reemplaza estos valores por los que encontrarás en tu panel de Supabase (Project Settings > API)
+const supabaseUrl = 'PUBLIC_SUPABASE_URL=https://ljxwsmhkntyqsklrxenx.supabase.co';
+const supabaseAnonKey = 'PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_7XEGBLy4rB9IDFyRds18jQ_Jwmzl1Oj';
+
