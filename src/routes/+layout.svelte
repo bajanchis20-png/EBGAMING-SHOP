12cd6f428b17a48b3e318644ef4e60c009d3995a
bajanchis20-png <Bajanchis20@gmail.com>
2026-08-04 19:16:09 -0400

    go

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import "./layout.css";
-    import favicon from "$lib/assets/logo2.png";
+    import favicon from "$lib/assets/logo3.png";
     import Icon from "@iconify/svelte";
 
     let { children } = $props();
